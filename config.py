ee497a5652aac78920e22625e2e6f240d09ff22d
Visncent <100198781+Visncent@users.noreply.github.com>
2025-05-15 05:09:51 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+OFFICIAL_DLC = {
+    "ludeon.rimworld.royalty": "Royalty DLC",
+    "ludeon.rimworld.ideology": "Ideology DLC",
+    "ludeon.rimworld.biotech": "Biotech DLC"
+}
+
+CRITICAL_MODS = {
+    "ludeon.rimworld": "Core Game",
+    "brrainz.harmony": "Harmony Library",
+    "unlimitedhugs.hugslib": "HugsLib",
+    "kentington.saveourship2": "Save Our Ship 2",
+    **OFFICIAL_DLC
+}
